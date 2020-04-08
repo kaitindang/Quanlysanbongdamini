e411cfcfd4e58536de18780b7d2e8b858dd2b1da
trongtinzsz <62632383+trongtinzsz@users.noreply.github.com>
2020-04-08 20:48:23 +0700

    Add files via upload
    file javascript

---
+++ javascipt/sandadat.js
@@ -0,0 +1,278 @@
+$(document).ready(function(){
+	//đánh dấu sân 1
+	$("#san17").click(function(){
+		$("#san17").css("background-color", "red");
+	});
+	$("#san18").click(function(){
+		$("#san18").css("background-color", "red");
+	});
+	$("#san19").click(function(){
+		$("#san19").css("background-color", "red");
+	});
+	$("#san110").click(function(){
+		$("#san110").css("background-color", "red");
+	});
+	$("#san111").click(function(){
+		$("#san111").css("background-color", "red");
+	});
+	$("#san112").click(function(){
+		$("#san112").css("background-color", "red");
+	});
+	$("#san113").click(function(){
+		$("#san113").css("background-color", "red");
+	});
+	$("#san114").click(function(){
+		$("#san114").css("background-color", "red");
+	});
+	$("#san115").click(function(){
+		$("#san115").css("background-color", "red");
+	});
+	$("#san116").click(function(){
+		$("#san116").css("background-color", "red");
+	});
+	$("#san117").click(function(){
+		$("#san117").css("background-color", "red");
+	});
+	$("#san118").click(function(){
+		$("#san118").css("background-color", "red");
+	});
+	$("#san119").click(function(){
+		$("#san119").css("background-color", "red");
+	});
+	$("#san120").click(function(){
+		$("#san120").css("background-color", "red");
+	});
+	$("#san121").click(function(){
+		$("#san121").css("background-color", "red");
+	});
+	//đánh dấu sân 2
+	$("#san27").click(function(){
+		$("#san27").css("background-color", "red");
+	});
+	$("#san28").click(function(){
+		$("#san28").css("background-color", "red");
+	});
+	$("#san29").click(function(){
+		$("#san29").css("background-color", "red");
+	});
+	$("#san210").click(function(){
+		$("#san210").css("background-color", "red");
+	});
+	$("#san211").click(function(){
+		$("#san211").css("background-color", "red");
+	});
+	$("#san212").click(function(){
+		$("#san212").css("background-color", "red");
+	});
+	$("#san213").click(function(){
+		$("#san213").css("background-color", "red");
+	});
+	$("#san214").click(function(){
+		$("#san214").css("background-color", "red");
+	});
+	$("#san215").click(function(){
+		$("#san215").css("background-color", "red");
+	});
+	$("#san216").click(function(){
+		$("#san216").css("background-color", "red");
+	});
+	$("#san217").click(function(){
+		$("#san217").css("background-color", "red");
+	});
+	$("#san218").click(function(){
+		$("#san218").css("background-color", "red");
+	});
+	$("#san219").click(function(){
+		$("#san219").css("background-color", "red");
+	});
+	$("#san220").click(function(){
+		$("#san220").css("background-color", "red");
+	});
+	$("#san221").click(function(){
+		$("#san221").css("background-color", "red");
+	});
+	//đánh dấu sân 3
+	$("#san37").click(function(){
+		$("#san37").css("background-color", "red");
+	});
+	$("#san38").click(function(){
+		$("#san38").css("background-color", "red");
+	});
+	$("#san39").click(function(){
+		$("#san39").css("background-color", "red");
+	});
+	$("#san310").click(function(){
+		$("#san310").css("background-color", "red");
+	});
+	$("#san311").click(function(){
+		$("#san311").css("background-color", "red");
+	});
+	$("#san312").click(function(){
+		$("#san312").css("background-color", "red");
+	});
+	$("#san313").click(function(){
+		$("#san313").css("background-color", "red");
+	});
+	$("#san314").click(function(){
+		$("#san314").css("background-color", "red");
+	});
+	$("#san315").click(function(){
+		$("#san315").css("background-color", "red");
+	});
+	$("#san316").click(function(){
+		$("#san316").css("background-color", "red");
+	});
+	$("#san317").click(function(){
+		$("#san317").css("background-color", "red");
+	});
+	$("#san318").click(function(){
+		$("#san318").css("background-color", "red");
+	});
+	$("#san319").click(function(){
+		$("#san319").css("background-color", "red");
+	});
+	$("#san320").click(function(){
+		$("#san320").css("background-color", "red");
+	});
+	$("#san321").click(function(){
+		$("#san321").css("background-color", "red");
+	});
+	//đánh dấu sân 4
+	$("#san47").click(function(){
+		$("#san47").css("background-color", "red");
+	});
+	$("#san48").click(function(){
+		$("#san48").css("background-color", "red");
+	});
+	$("#san49").click(function(){
+		$("#san49").css("background-color", "red");
+	});
+	$("#san410").click(function(){
+		$("#san410").css("background-color", "red");
+	});
+	$("#san411").click(function(){
+		$("#san411").css("background-color", "red");
+	});
+	$("#san412").click(function(){
+		$("#san412").css("background-color", "red");
+	});
+	$("#san413").click(function(){
+		$("#san413").css("background-color", "red");
+	});
+	$("#san414").click(function(){
+		$("#san414").css("background-color", "red");
+	});
+	$("#san415").click(function(){
+		$("#san415").css("background-color", "red");
+	});
+	$("#san416").click(function(){
+		$("#san416").css("background-color", "red");
+	});
+	$("#san417").click(function(){
+		$("#san417").css("background-color", "red");
+	});
+	$("#san418").click(function(){
+		$("#san418").css("background-color", "red");
+	});
+	$("#san419").click(function(){
+		$("#san419").css("background-color", "red");
+	});
+	$("#san420").click(function(){
+		$("#san420").css("background-color", "red");
+	});
+	$("#san421").click(function(){
+		$("#san421").css("background-color", "red");
+	});
+	//đánh dấu sân 5
+	$("#san57").click(function(){
+		$("#san57").css("background-color", "red");
+	});
+	$("#san58").click(function(){
+		$("#san58").css("background-color", "red");
+	});
+	$("#san59").click(function(){
+		$("#san59").css("background-color", "red");
+	});
+	$("#san510").click(function(){
+		$("#san510").css("background-color", "red");
+	});
+	$("#san511").click(function(){
+		$("#san511").css("background-color", "red");
+	});
+	$("#san512").click(function(){
+		$("#san512").css("background-color", "red");
+	});
+	$("#san513").click(function(){
+		$("#san513").css("background-color", "red");
+	});
+	$("#san514").click(function(){
+		$("#san514").css("background-color", "red");
+	});
+	$("#san515").click(function(){
+		$("#san515").css("background-color", "red");
+	});
+	$("#san516").click(function(){
+		$("#san516").css("background-color", "red");
+	});
+	$("#san517").click(function(){
+		$("#san517").css("background-color", "red");
+	});
+	$("#san518").click(function(){
+		$("#san518").css("background-color", "red");
+	});
+	$("#san519").click(function(){
+		$("#san519").css("background-color", "red");
+	});
+	$("#san520").click(function(){
+		$("#san520").css("background-color", "red");
+	});
+	$("#san521").click(function(){
+		$("#san521").css("background-color", "red");
+	});
+	//đánh dấu sân 6
+	$("#san67").click(function(){
+		$("#san67").css("background-color", "red");
+	});
+	$("#san68").click(function(){
+		$("#san68").css("background-color", "red");
+	});
+	$("#san69").click(function(){
+		$("#san69").css("background-color", "red");
+	});
+	$("#san610").click(function(){
+		$("#san610").css("background-color", "red");
+	});
+	$("#san611").click(function(){
+		$("#san611").css("background-color", "red");
+	});
+	$("#san612").click(function(){
+		$("#san612").css("background-color", "red");
+	});
+	$("#san613").click(function(){
+		$("#san613").css("background-color", "red");
+	});
+	$("#san614").click(function(){
+		$("#san614").css("background-color", "red");
+	});
+	$("#san615").click(function(){
+		$("#san615").css("background-color", "red");
+	});
+	$("#san616").click(function(){
+		$("#san616").css("background-color", "red");
+	});
+	$("#san617").click(function(){
+		$("#san617").css("background-color", "red");
+	});
+	$("#san618").click(function(){
+		$("#san618").css("background-color", "red");
+	});
+	$("#san619").click(function(){
+		$("#san619").css("background-color", "red");
+	});
+	$("#san620").click(function(){
+		$("#san620").css("background-color", "red");
+	});
+	$("#san621").click(function(){
+		$("#san621").css("background-color", "red");
+	});
+});
